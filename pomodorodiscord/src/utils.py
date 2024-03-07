73819b1f96390d477fc056bec55e3dbcc349a439
freeram <kulikovpavel2801@gmail.com>
2024-03-07 08:11:29 -0700

    Format

--- pomodorodiscord/src/utils.py
+++ pomodorodiscord/src/utils.py
@@ -6,6 +6,7 @@ from pygame import mixer
 mixer.init()
 beep = mixer.Sound('sounds/beep.mp3')
 
+# Until user sets custom pomodoro, short break, long break durations, this gets set
 DEF_POMODORO_MINS = 25
 DEF_SB_MINS = 5
 DEF_LB_MINS = 10
@@ -20,15 +21,19 @@ def load_file(filename, on_no_file=None):
 
     return data
 
-def load_config():
-    return load_file('config.json', {'theme': 'Default'})
 
 def load_data():
     return load_file('data.json', None)
 
+
+def load_config():
+    return load_file('config.json', {'theme': 'Default', 'sound': 'beep.mp3'})
+
+
 def save_config(config):
     with open('config.json', 'w') as config_file:
         json.dump(config, config_file, indent=4)
+
 
 def reload_app():
     os.execl(sys.executable, sys.executable, *sys.argv)
