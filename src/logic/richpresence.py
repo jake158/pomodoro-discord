58fbbe9f7e3cae6812571e6a010560e1eba92327
freeram <kulikovpavel2801@gmail.com>
2024-05-10 20:35:26 -0600

    Improve time formatting

--- src/logic/richpresence.py
+++ src/logic/richpresence.py
@@ -46,17 +46,18 @@ class RichPresence(pypresence.Presence):
             self.connected = True
             return False
 
-    def format_time(self, seconds_studied, round_hours_to=1):
+    def format_time(self, seconds_studied, float_hours=True):
         total_seconds = seconds_studied
         total_hours = seconds_studied / 3600
         if total_hours < 1:
             return f"{total_seconds // 60} minute{'s' if total_seconds // 60 != 1 else ''}"
         else:
-            return f"{round(total_hours, round_hours_to) if total_hours % 1 != 0 else int(total_hours)} hours"
+            formatted_hours = round(total_hours, 1) if float_hours and total_hours % 1 != 0 else int(total_hours)
+            return f"{formatted_hours} hour{'s' if formatted_hours != 1 else ''}"
 
     @_handle_exceptions
     def idling_state(self, seconds_studied=0):
-        self.update(state=f"Total time studied: {self.format_time(self.total_seconds_studied + seconds_studied, None)}", 
+        self.update(state=f"Total time studied: {self.format_time(self.total_seconds_studied + seconds_studied, False)}", 
                     details="Idling",
                     start=self.launch_time, 
                     large_image="graytomato", 
